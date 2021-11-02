5d26eea6dcf2d1702fced1d822d08750a1e7fdee
jabialowas <bialowas.ja@gmail.com>
2021-11-02 04:34:14 +0100

    Better formatting

--- main.js
+++ main.js
@@ -1,55 +1,49 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Discord = require('discord.js')
-const fs = require('fs');
+const Discord = require("discord.js");
+const fs = require("fs");
 
 const client = new Discord.Client({
-	allowedMentions: {
-		parse: ['users', 'roles'],
-		repliedUser: true,
-	},
-	
-	intents: [
-		"GUILDS",
-		"GUILD_MESSAGES",
-		"GUILD_PRESENCES",
-		"GUILD_MEMBERS",
-		"GUILD_MESSAGE_REACTIONS"
-	]
+  allowedMentions: {
+    parse: ["users", "roles"],
+    repliedUser: true,
+  },
+
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES",
+    "GUILD_PRESENCES",
+    "GUILD_MEMBERS",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
 });
 
-
-const prefix = '!';
-
-
+const prefix = "!";
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
 
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.once('ready', () => { 
-    console.log('Bot rdy!');
+client.once("ready", () => {
+  console.log("Bot rdy!");
 });
 
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'help'){
-        client.commands.get('help').execute(message, args, Discord);
-    }else if (command === 'dump'){
-		client.commands.get('dump').execute(message, args);
-	}	
-	
+  if (command === "help") {
+    client.commands.get("help").execute(message, args, Discord);
+  } else if (command === "dump") {
+    client.commands.get("dump").execute(message, args);
+  }
 });
-
-
-
 
 client.login(process.env.BOT_TOKEN);
